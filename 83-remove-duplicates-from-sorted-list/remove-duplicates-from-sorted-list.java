c5364d23fc59621ba8c6d0233175690916ec8db2
Priyadharshini Sundarrajan <2k23it42@kiot.ac.in>
2026-07-18 07:31:55 +0530

    Time: 0 ms (100.00%) | Memory: 45.4 MB (69.80%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.java
@@ -8,25 +8,43 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+// class Solution {
+//     public ListNode deleteDuplicates(ListNode head) {
+
+//         if (head == null) return null;
+
+//         ListNode temp1 = head;
+//         ListNode temp2 = head.next;
+
+//         while (temp2 != null) {
+
+//             if (temp1.val == temp2.val) {
+//                 temp1.next = temp2.next;
+//                 temp2 = temp2.next;
+//             } 
+//             else {
+//                 temp1 = temp2;
+//                 temp2 = temp2.next;
+//             }
+//         }
+//         return head;
+//     }
+// }
+
 class Solution {
     public ListNode deleteDuplicates(ListNode head) {
 
-        if (head == null) return null;
+        ListNode current = head;
 
-        ListNode temp1 = head;
-        ListNode temp2 = head.next;
+        while (current != null && current.next != null) {
 
-        while (temp2 != null) {
-
-            if (temp1.val == temp2.val) {
-                temp1.next = temp2.next;
-                temp2 = temp2.next;
-            } 
-            else {
-                temp1 = temp2;
-                temp2 = temp2.next;
+            if (current.val == current.next.val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
             }
         }
+
         return head;
     }
 }
