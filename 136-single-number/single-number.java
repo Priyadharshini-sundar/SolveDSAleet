b8436980a01607e0af65976207fbaee0f2261ebd
Priyadharshini Sundarrajan <2k23it42@kiot.ac.in>
2026-07-21 07:16:38 +0530

    Time: 1 ms (99.96%) | Memory: 46.6 MB (92.86%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,15 +1,26 @@
-import java.util.*;
+// import java.util.*;
+// class Solution {
+//     public int singleNumber(int[] nums) {
+//         HashMap <Integer, Integer> map = new HashMap<>();
+//         for(int i = 0; i < nums.length; i++){
+//             map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             if (map.get(nums[i]) == 1) {
+//                 return nums[i];
+//             }
+//         }
+//         return -1;
+//     }
+// }
 class Solution {
     public int singleNumber(int[] nums) {
-        HashMap <Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        int ans = 0;
+
+        for (int num : nums) {
+            ans ^= num;
         }
-        for (int i = 0; i < nums.length; i++) {
-            if (map.get(nums[i]) == 1) {
-                return nums[i];
-            }
-        }
-        return -1;
+
+        return ans;
     }
 }
