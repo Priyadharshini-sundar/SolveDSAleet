f87764322a3ad12f31cbc194393b648a930eca96
Priyadharshini Sundarrajan <2k23it42@kiot.ac.in>
2026-07-22 21:28:20 +0530

    Time: 1 ms (97.94%) | Memory: 42.5 MB (63.24%) - LeetSync

--- 258-add-digits/add-digits.java
+++ 258-add-digits/add-digits.java
@@ -1,15 +1,21 @@
+// class Solution {
+//     public int addDigits(int num) {
+        
+//         while(num > 9){
+//             int sum = 0;
+//             while(num > 0){
+//                 int digit = num % 10;
+//                 sum += digit;
+//                 num /= 10;
+//             }
+//             num = sum;
+//         }
+//         return num;
+//     }
+// }
+
 class Solution {
     public int addDigits(int num) {
-        
-        while(num > 9){
-            int sum = 0;
-            while(num > 0){
-                int digit = num % 10;
-                sum += digit;
-                num /= 10;
-            }
-            num = sum;
-        }
-        return num;
+        return 1 + (num - 1) % 9;
     }
 }
