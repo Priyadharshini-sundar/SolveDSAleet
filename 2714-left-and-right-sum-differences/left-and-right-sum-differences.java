413d41254d97c959a1361e43a70eaff24de721c1
Priyadharshini Sundarrajan <2k23it42@kiot.ac.in>
2026-07-27 10:02:12 +0530

    Time: 2 ms (98.48%) | Memory: 46.5 MB (73.83%) - LeetSync

--- 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
+++ 2714-left-and-right-sum-differences/left-and-right-sum-differences.java
@@ -1,25 +1,46 @@
+// class Solution {
+//     public int[] leftRightDifference(int[] nums) {
+//         int n = nums.length;
+
+//         int [] rightSum = new int[n];
+//         rightSum[n - 1] = 0;
+//         for(int i = n - 2; i >= 0; i--){
+//             rightSum[i] = rightSum[i + 1] + nums[i + 1];
+//         }
+
+//         int [] leftSum = new int[n];
+//         leftSum[0] = 0;
+//         for(int i = 1; i < n; i++){
+//             leftSum[i] = leftSum[i - 1] + nums[i - 1];
+//         }
+
+//         int[] ans = new int[n];
+//         for (int i = 0; i < n; i++) {
+//             ans[i] = Math.abs(leftSum[i] - rightSum[i]);
+//         }
+
+//         return ans;
+//     }
+// }
+    
 class Solution {
     public int[] leftRightDifference(int[] nums) {
         int n = nums.length;
-
-        int [] rightSum = new int[n];
-        rightSum[n - 1] = 0;
-        for(int i = n - 2; i >= 0; i--){
-            rightSum[i] = rightSum[i + 1] + nums[i + 1];
-        }
-
-        int [] leftSum = new int[n];
-        leftSum[0] = 0;
-        for(int i = 1; i < n; i++){
-            leftSum[i] = leftSum[i - 1] + nums[i - 1];
-        }
-
         int[] ans = new int[n];
+
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+
         for (int i = 0; i < n; i++) {
-            ans[i] = Math.abs(leftSum[i] - rightSum[i]);
+            int rightSum = totalSum - leftSum - nums[i];
+            ans[i] = Math.abs(leftSum - rightSum);
+            leftSum += nums[i];
         }
 
         return ans;
     }
 }
-    
