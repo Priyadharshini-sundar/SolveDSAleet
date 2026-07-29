ba9af47db0c1c8a4589450f8e030ee6a12bf6180
Priyadharshini Sundarrajan <2k23it42@kiot.ac.in>
2026-07-29 08:20:10 +0530

    Time: 17 ms (41.02%) | Memory: 43.7 MB (86.95%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -4,21 +4,27 @@ class Solution {
             return false;
         }
 
-        HashMap<Character, Character> map = new HashMap<>();
+        HashMap<Character, Character> map1 = new HashMap<>();
+        HashMap<Character, Character> map2 = new HashMap<>();
 
         for (int i = 0; i < s.length(); i++) {
             char ch1 = s.charAt(i);
             char ch2 = t.charAt(i);
 
-            if (map.containsKey(ch1)) {
-                if (map.get(ch1) != ch2) {
+            if (map1.containsKey(ch1)) {
+                if (map1.get(ch1) != ch2) {
                     return false;
                 }
             } else {
-                if (map.containsValue(ch2)) {
+                map1.put(ch1, ch2);
+            }
+
+            if (map2.containsKey(ch2)) {
+                if (map2.get(ch2) != ch1) {
                     return false;
                 }
-                map.put(ch1, ch2);
+            } else {
+                map2.put(ch2, ch1);
             }
         }
 
